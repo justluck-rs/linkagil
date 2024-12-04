6ddc8db4564efbeee3f29c12e73ba1446ecd063c
justluck-rs <luckcastle@outlook.com>
2024-12-04 15:22:33 -0300

    feat(list-professional): lista de cards de profissionais

--- src/views/layouts/home/Main.vue
+++ src/views/layouts/home/Main.vue
@@ -1,21 +1,16 @@
 <template>
-  <div>
-    <Navbar/>
-
-    <!-- <Aside/> -->
-
-    <main class="h-screen">
-      <slot>
-        <Card/>
-      </slot>
+  <section class="relative flex flex-col">
+    <Navbar />
+    <main class="h-screen px-64 flex-1">
+      <List />
     </main>
 
-    <Footer/>
-  </div>
+    <Footer />
+  </section>
 </template>
 
 <script setup>
   import Navbar from './components/navbar/Main.vue'
   import Footer from './components/footer/Main.vue'
-  import Card from './professional/card/Main.vue'
+import List from './professional/list/Main.vue'
 </script>
---
+++ src/views/layouts/home/professional/list/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="grid grid-cols-3 gap-3 p-10 bg-white mt-14">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </section>
+</template>
+
+<script setup>
+import Card from '../card/Main.vue'
+</script>
